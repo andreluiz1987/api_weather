ea53beb78a419a26c7a380196f8a88ed37f82c38
RabBit_BR <andre.luiz1987@gmail.com>
2019-09-05 12:54:38 -0300

    Update accurWeatherService.js

--- src/service/accurWeatherService.js
+++ src/service/accurWeatherService.js
@@ -3,7 +3,9 @@ var fahrenheitToCelsius = require('fahrenheit-to-celsius');
 const Weather = require("../models/weatherModel");
 
 exports.getWeather = () => {
-    return axios.get('http://dataservice.accuweather.com/forecasts/v1/daily/1day/44403?apikey=FA8zLQNeMBQ4dTkp2uKxweY6OQPHHgWK')
+    let token = process.env.ACCUR_WEATHER_TOKEN;
+    let url = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/44403?apikey=' + token;
+    return axios.get(url)
         .then(response => {
             let weather = new Weather("Belo Horizonte",
                 parseInt(fahrenheitToCelsius(response.data.DailyForecasts[0].Temperature.Maximum.Value)),
