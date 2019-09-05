311d154d76927fae4846d606b2d664695aef2b1d
RabBit_BR <andre.luiz1987@gmail.com>
2019-09-05 19:39:25 -0300

    Update openWeatherMapService.js

--- src/service/openWeatherMapService.js
+++ src/service/openWeatherMapService.js
@@ -3,18 +3,18 @@ const kelvinToCelsius = require('kelvin-to-celsius');
 const Weather = require("../models/weatherModel");
 
 exports.getWeather = () => {
-    let url - 'https://api.openweathermap.org/data/2.5/weather?id=2270968&appid=8506ad2c74c3c69fbdd1708ec177fdf2';
-    return axios.get(url)
-        .then(response => {
-            let code = response.data.cod;
-            if (code == 200) {
-                let weather = new Weather(response.data.name,
-                    kelvinToCelsius(response.data.main.temp),
-                    response.data.main.humidity);
-                return weather;
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
+//     let url - 'https://api.openweathermap.org/data/2.5/weather?id=2270968&appid=8506ad2c74c3c69fbdd1708ec177fdf2';
+//     return axios.get(url)
+//         .then(response => {
+//             let code = response.data.cod;
+//             if (code == 200) {
+//                 let weather = new Weather(response.data.name,
+//                     kelvinToCelsius(response.data.main.temp),
+//                     response.data.main.humidity);
+//                 return weather;
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
 }
