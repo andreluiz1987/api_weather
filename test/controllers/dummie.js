c53e31a03547b587d88b7cc6b311e3a6d023611a
andreluiz1987 <andre.coelho@hotmart.com>
2019-09-25 19:25:49 -0300

    fixing test

--- test/controllers/dummie.js
+++ test/controllers/dummie.js
@@ -2,6 +2,6 @@ const assert = require("assert");
 
 describe("smoke test", function() {
   it("checks equality", function() {
-    assert.equal(true, false);
+    assert.equal(true, true);
   });
 });
